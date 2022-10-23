5e10065a5f2ba0ecaa08b5c4965ed3e20d01ae59
Alex Lynd <22926680+AlexLynd@users.noreply.github.com>
2022-10-23 01:33:54 -0700

    Update animations.py

--- animations.py
+++ animations.py
@@ -7,6 +7,7 @@ from adafruit_led_animation.animation.rainbow import Rainbow
 from adafruit_led_animation.animation.rainbowchase import RainbowChase
 from adafruit_led_animation.animation.rainbowcomet import RainbowComet
 
+from adafruit_led_animation.sequence import AnimationSequence
 
 from adafruit_led_animation.color import *
 
@@ -23,14 +24,16 @@ rainbow_chase = RainbowChase(pixels, speed=0.1, size=5, spacing=3, step=50)
 rainbow_comet = RainbowComet(pixels, speed=0.1, tail_length=7, bounce=True)
 
 animations = AnimationSequence(
-    comet, blink, chase, advance_interval=5, auto_clear=True, random_order=True
+    rainbow_comet, rainbow_chase,sparkle_pulse, advance_interval=5, auto_clear=True, random_order=True
 )
 
 while True:
     #rainbow_chase.animate()
-    rainbow_comet.animate()
+    #rainbow_comet.animate()
     #sparkle_pulse.animate()
     #rainbow.animate()
     
-    String together animations
-    #animations.animate()
+    #String together animations
+    animations.animate()
+
+    
